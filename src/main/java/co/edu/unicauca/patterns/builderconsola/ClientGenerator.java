b44666a475f80061b2bd32feac9183cc3286d9b4
juanbep <56483953+juanbep@users.noreply.github.com>
2021-04-14 20:51:40 -0500

    versión 1.0

---
+++ src/main/java/co/edu/unicauca/patterns/builderconsola/ClientGenerator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicauca.patterns.builderconsola;
+
+/**
+ *
+ * @author Beca98
+ */
+public class ClientGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/co/edu/unicauca/patterns/restaurantebuilder/Dish.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicauca.patterns.restaurantebuilder;
+
+import java.util.List;
+
+/**
+ *
+ * @author Beca98
+ */
+public class Dish {
+
+    private String name;
+    private String description;
+    private String image;
+    private int price;
+    private EnumSize size;
+    private List<Dish> parts;
+   
+    
+    
+    
+   
+}
